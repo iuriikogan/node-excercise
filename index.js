36ab6a3981da21287d58f91a22aca8b473f2da4e
Iurii Kogan <koganiurii@gmail.com>
2019-04-15 12:39:23 +0700

    comments

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express()
 app.get('/', function(request, response, next) {
     response.send('<H1>Hello this is my live Server</H1>') 
 })
-
+app.get('/about.html', function(request, response, next) {
+    response.send('<H1>ABOUT US</H1>') 
+})
 //keep at the end of the file
 app.listen(3000, function(){
     console.log("server is running on port 3000");
