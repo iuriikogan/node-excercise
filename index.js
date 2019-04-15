075b317e4c689ce9abc915271a8a30f580c38e5a
Iurii Kogan <koganiurii@gmail.com>
2019-04-15 12:33:00 +0700

    live nodemon server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 
 const app = express()
+//code here
 
+app.get('/', function(request, response, next) {
+    response.send('<H1>Hello this is my live Server</H1>') 
+})
+
+//keep at the end of the file
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 })
 
 // in bash node index.html starts server on port 3000;
+
+// in bash nodemon index.js starts live server
