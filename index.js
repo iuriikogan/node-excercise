e6986d214ca38ebb319519ae4bb887c0c90c25ea
Iurii Kogan <koganiurii@gmail.com>
2019-04-15 15:38:22 +0700

    body-parser

--- index.js
+++ index.js
@@ -1,25 +1,90 @@
 const express = require('express')
 
 const app = express()
+
+const body_parser = require('body-parser')
 //code here
 
-app.get('/', function(request, response, next) {
-    response.send('<H1>Hello this is my live Server</H1>') 
+app.use(body_parser.json())
+
+// testJSON
+var channels = [{
+        id: 1,
+        name: 'General'
+    }, {
+        id: 2,
+        name: 'Random'
+    }, {
+        id: 3,
+        name: 'Private'
+    },
+    {
+        id: 2,
+        name: 'fun'
+    }];
+
+var messages = [{
+    id: 1,
+    name: 'General',
+    content: [{
+        name: 'message1',
+        name: 'message2',
+        name: 'message3'
+    }]
+}, {
+    id: 2,
+    name: 'random',
+    content: {
+       
+    }
+}];
+
+app.get('/', function (request, response, next) {
+    response.send('<H1>Hello this is my live Server</H1>');
 })
-    // insert HTML
-app.get('/about', function(request, response, next) {
-    response.send('<H1>About Us</H1>') 
+// insert HTML
+app.get('/about', function (request, response, next) {
+    response.send('<H1>About Us</H1>');
+
 })
-    // send file
-app.get('/contact', function(request, response, next) {
-    response.sendFile(__dirname + '/contact.html'); 
+// response .html page renders
+app.get('/contact', function (request, response, next) {
+    response.sendFile(__dirname + '/contact.html');
 })
-    // Download
-app.get('/file', function(request, response, next) {
-    response.download(__dirname + '/notes.txt'); 
+// Download
+app.get('/file', function (request, response, next) {
+    response.download(__dirname + '/notes.txt');
 })
+
+
+app.get('/channel/:id', function (request, response, next) {
+    let channel = channels.find(function (c) {
+        return c.id == request.params.id;
+    });
+    response.send(channel);
+})
+app.post('/channels', function (request, response, next) {
+    console.log(request.body);
+})
+
+app.get('/messages/:id', function (request, response, next) {
+    let message = messages.content.find(function (c) {
+        return c.id == request.params.id;
+    });
+    response.send(message);
+})
+app.get('/messages', function (request, response, next) {
+    response.send(messages);
+})
+
+
+
+
+
+
+
 //keep at the end of the file
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server is running on port 3000");
 })
 
